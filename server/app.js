eb1a4de54450131fd4417641ec4a282df1891570
labor-day <shaffer.scott@yahoo.com>
2022-08-29 23:36:08 -0600

    optimized queries

--- server/app.js
+++ server/app.js
@@ -14,20 +14,17 @@ const { Op } = require("sequelize");
 // Express using json - DO NOT MODIFY
 app.use(express.json());
 
-
-
-
 // STEP #Ob: Test logging behavior - DO NOT MODIFY
 app.get('/test-benchmark-logging', async (req, res) => {   // > 100 ms execution time
     const books = await Book.findAll({
         include: [
-            { model: Author }, 
+            { model: Author },
             { model: Review },
             { model: Reviewer }
         ],
         // Uncomment the lines below to see the data structure more clearly
-        // limit: 100,
-        // offset: 2000
+        limit: 100,
+        offset: 2000
     });
     res.json(books);
 });
@@ -36,14 +33,35 @@ app.get('/test-benchmark-logging', async (req, res) => {   // > 100 ms execution
 // STEP #1: Benchmark a Frequently-Used Query
 app.get('/books', async (req, res) => {
 
-    let books = await Book.findAll({
-        include: Author,
-    });
+    //this method takes 70-90 ms because we filter our books after querying
+    // let books = await Book.findAll({
+    //     include: Author,
+    // });
 
-    // Filter by price if there is a maxPrice defined in the query params
+    // // Filter by price if there is a maxPrice defined in the query params
+    // if (req.query.maxPrice) {
+    //     books = books.filter(book => book.price < parseInt(req.query.maxPrice));
+    // };
+    // res.json(books);
+
+    //filter at the same time as querying = 25-30ms
+    let books;
     if (req.query.maxPrice) {
-        books = books.filter(book => book.price < parseInt(req.query.maxPrice));
-    };
+        books = await Book.findAll({
+            include: Author,
+            where: {
+                price: {
+                    [Op.lt]: req.query.maxPrice
+                }
+            }
+        });
+    } else {
+        books = await Book.findAll({
+            include: Author
+        });
+    }
+
+
     res.json(books);
 });
 
@@ -54,8 +72,8 @@ app.get('/books', async (req, res) => {
         // - What is happening in the code of the query itself?
 
 
-        // - What exactly is happening as SQL executes this query? 
- 
+        // - What exactly is happening as SQL executes this query?
+
 
 
 
@@ -79,36 +97,65 @@ app.get('/books', async (req, res) => {
 
 
 // STEP #2: Benchmark and Refactor Another Query
-app.patch('/authors/:authorId/books', async (req, res) => {
-    const author = await Author.findOne({
-        include: { model: Book },
-        where: {
-            id: req.params.authorId
-        }
-    });
+// app.patch('/authors/:authorId/books', async (req, res) => {
+//     const author = await Author.findOne({
+//         include: { model: Book },
+//         where: {
+//             id: req.params.authorId
+//         }
+//     });
 
-    if (!author) {
-        res.status(404);
-        return res.json({
-            message: 'Unable to find an author with the specified authorId'
-        });
+//     if (!author) {
+//         res.status(404);
+//         return res.json({
+//             message: 'Unable to find an author with the specified authorId'
+//         });
+//     }
+
+//     for (let book of author.Books) {
+//         book.price = req.body.price;
+//         await book.save();
+//     }
+
+//     const books = await Book.findAll({
+//         where: {
+//             authorId: author.id
+//         }
+//     });
+
+//     res.json({
+//         message: `Successfully updated all authors.`,
+//         books
+//     });
+
+    //current method takes 12 * 4 = 48 ms
+//});
+
+app.patch('/authors/:authorId/books', async (req, res, next) => {
+
+    try {
+        await Book.update(
+            {price: 19.99},
+            {where: {
+                authorId: {
+                    [Op.eq]: req.params.authorId
+                }
+            }}
+        );
+        let books = await Book.findAll({
+            where: {
+                authorId: {
+                    [Op.eq]: req.params.authorId
+                }
+            }
+        })
+        res.json({message: `Successfully updated all authors.`, books});
+    } catch(err) {
+        res.json({message: 'Unable to find an author with the specified authorId' });
     }
 
-    for (let book of author.Books) {
-        book.price = req.body.price;
-        await book.save();
-    }
+    //8 ms
 
-    const books = await Book.findAll({
-        where: {
-            authorId: author.id
-        }
-    });
-
-    res.json({
-        message: `Successfully updated all authors.`,
-        books
-    });
 });
 
 
@@ -119,6 +166,7 @@ app.patch('/authors/:authorId/books', async (req, res) => {
     // GET /reviews?firstName=Daisy&lastName=Herzog
     // GET /reviews?firstName=Daisy
     // GET /reviews?lastName=Herzog
+    // initial implementation takes 6ms, 2ms, 1ms
 app.get('/reviews', async (req, res) => {
     const { firstName, lastName } = req.query;
 
@@ -129,7 +177,7 @@ app.get('/reviews', async (req, res) => {
 
     const reviews = await Review.findAll({
         include: {
-            model: Reviewer, 
+            model: Reviewer,
             where: whereClause,
             attributes: ['firstName', 'lastName']
         },
